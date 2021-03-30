44f01609277063f85c4115636539e58e271ab267
charindu <charindu87@gmail.com>
2021-03-30 22:45:51 +0800

    Adding admin lte framework

--- pharm-ui/src/App.js
+++ pharm-ui/src/App.js
@@ -1,28 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./component/Header";
+import Menu from "./component/Menu";
+import Content from "./component/Content";
+import Footer from "./component/Footer";
 
 function App () {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch('/api/hello')
-        .then(response => response.text())
-        .then(message => {
-          setMessage(message);
-        });
-  },[])
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">{message}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-  )
+    return(
+        <div>
+            <Header/>
+            <Menu/>
+            <Content/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
