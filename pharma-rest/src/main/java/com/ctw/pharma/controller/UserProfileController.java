62c3895dbefce0bea887d648ede970aa083c064f
charindu <charindu87@gmail.com>
2021-04-04 18:36:10 +0800

    ENH : User registration and listing

--- pharma-rest/src/main/java/com/ctw/pharma/controller/UserProfileController.java
+++ pharma-rest/src/main/java/com/ctw/pharma/controller/UserProfileController.java
@@ -7,9 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,16 +24,17 @@ public class UserProfileController {
     @PostMapping( value = "/create")
     public ResponseEntity<?> createUserProfile(@RequestBody User user){
         LOGGER.info("UserProfileController:create: " + user);
-        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> map = new HashMap<>();
         user = userService.createUser(user);
         map.put("user", user);
-        return ResponseEntity.ok(user);
+        map.put("isSuccess", true);
+        return ResponseEntity.ok(map);
     }
 
     @GetMapping( value = "/userslisting")
     public ResponseEntity<?> listingAllActiveUsers(){
         LOGGER.info("UserProfileController:listingAllActiveUsers");
-        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> map = new HashMap<>();
         List<User> activeUsers = userService.loadAllActiveUsers();
         map.put("activeUsers", activeUsers);
         return ResponseEntity.ok(map);
